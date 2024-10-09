018e5b566241cc9de08fafb31c82105ee00bb4a8
DangThiThuyDung06 <thuydungaelita@gmail.com>
2024-10-09 21:57:07 +0700

    add connect cam

--- train_data/connect_webcam/connect_camera.py
+++ train_data/connect_webcam/connect_camera.py
@@ -1,13 +1,24 @@
 import cv2
 import numpy as np
+import argparse
+
+
+def parse_args() -> argparse.Namespace:
+    parse = argparse.ArgumentParser(description="Yolov8 live camera")
+    parse.add_argument("--webcam_resolutions", type=int, default=[1280, 720], nargs=2)
+    args = parse.parse_args()
+    return args
 
 
 def main():
+    args = parse_args()
     # Thay đổi đường dẫn source thành địa chỉ RTMP của bạn
-    rtmp_url = "rtmp://54.92.211.110:1915/live"
-
+    rtmp_url = "rtmp://54.92.211.110:1965/live"
+    frame_width, frame_height = args.webcam_resolutions
     # Khởi tạo VideoCapture với đường dẫn RTMP
     cap = cv2.VideoCapture(rtmp_url)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
     # cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
     # Kiểm tra xem việc mở luồng RTMP có thành công không
     # if not cap.isOpened():
@@ -23,9 +34,9 @@ def main():
         # Hiển thị khung hình
         cv2.imshow("YOLOv8 - RTMP Stream", frame)
 
-        # # Nhấn phím ESC (mã ASCII 27) để thoát khỏi cửa sổ
-        # if cv2.waitKey(1) == 27:
-        #     break
+        # Nhấn phím ESC (mã ASCII 27) để thoát khỏi cửa sổ
+        if cv2.waitKey(30) == 27:
+            break
 
     # Giải phóng tài nguyên sau khi kết thúc
     # cap.release()
