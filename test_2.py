dcdd46ec8b8e3f2605ea4832013ae83860a30ae1
DangThiThuyDung06 <thuydungaelita@gmail.com>
2024-12-05 21:59:05 +0700

    return link video

--- test_2.py
+++ test_2.py
@@ -1,9 +1,9 @@
 from yolo_model.controllers._upload_video_s3 import convert_video_to_mp4 as convert_mp4
 from yolo_model.controllers import _upload_video_s3
 
-file_path = "./file_path/video_stream\output_20241203_221002.mp4"
+file_path = "./file_path/video_stream/output_20241203_221002.mp4"
 
 uploaf = convert_mp4(file_path)
 
 
-out_put = _upload_video_s3.upload_video_to_s3(uploaf)
+out_put = _upload_video_s3.upload_video_to_s3_async(uploaf)
