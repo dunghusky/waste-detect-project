ffb1e1336475cb5b7a7163fb30dcdf5b21c3a63f
DangThiThuyDung06 <thuydungaelita@gmail.com>
2024-12-19 22:54:14 +0700

    crud data

--- database/models/DanhMucMoHinh.py
+++ database/models/DanhMucMoHinh.py
@@ -7,5 +7,7 @@ class DanhMucMoHinh(Base):
 
     maMoHinh = Column(Integer, primary_key=True, autoincrement=True)
     tenMoHinh = Column(String(1000), nullable=False)
+    duongDan = Column(String(1000), nullable=False)
+    ghiChu = Column(String(1000))
 
     videos = relationship("VideoXuLy", back_populates="danhmucmohinh")
