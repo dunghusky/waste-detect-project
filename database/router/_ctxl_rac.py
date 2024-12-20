dd36efb48ddcc3fd9a7fba623e2a50c426805b7c
DangThiThuyDung06 <thuydungaelita@gmail.com>
2024-12-20 23:18:32 +0700

    api update delete

--- database/router/_ctxl_rac.py
+++ database/router/_ctxl_rac.py
@@ -5,6 +5,7 @@ from database.dependencies.dependencies import get_db
 from sqlalchemy import text
 from sqlalchemy.orm import Session
 
+from database.schemas._ctxl_rac import ProcessWasteDelete, ProcessWasteUpdate
 from database.models.Camera import Camera
 from database.models.DanhMucPhanLoaiRac import DanhMucPhanLoaiRac
 from database.models.DanhMucMoHinh import DanhMucMoHinh
@@ -55,3 +56,90 @@ def get_video_detail_process_data(db: Session = Depends(get_db)):
         )
     except Exception as e:
         return JSONResponse({"status": 500, "message": f"Lỗi hệ thống! + {e}"})
+
+
+@router.post("/delete_details_waste")
+def delete_details_waste(request: ProcessWasteDelete, db: Session = Depends(get_db)):
+    try:
+        # Kiểm tra xem mã mô hình có tồn tại không
+        idCamera = request.idCamera
+        idWaste  = request.idWaste
+
+        camera = db.query(ChiTietXuLyRac).filter_by(maCamera=idCamera, maVideo=idWaste).first()
+        if not camera:
+            return JSONResponse(
+                content={
+                    "status": 404,
+                    "message": f"Mã {idCamera} {idWaste} không tồn tại.",
+                },
+                status_code=404,
+            )
+
+        # Xóa dòng trong bảng DanhMucMoHinh
+        db.delete(camera)
+        db.commit()
+
+        return JSONResponse(
+            content={
+                "status": 200,
+                "message": f"Xóa mã {idCamera} {idWaste} thành công.",
+            },
+            status_code=200,
+        )
+
+    except Exception as e:
+        return JSONResponse(
+            content={"status": 500, "message": f"Lỗi hệ thống: {str(e)}"},
+            status_code=500,
+        )
+
+
+@router.post("/update_details_wastes_data")
+def update_details_wastes_data(
+    request: ProcessWasteUpdate, db: Session = Depends(get_db)
+):
+    try:
+        data = request.dataProcessWaste
+        
+        id_video = data.get("maVideo")
+        id_waste = data.get("maRacThai")
+
+        if not id_video or not id_waste:
+            return JSONResponse(
+                content={"status": 400, "message": "Thiếu mã để cập nhật."},
+                status_code=400,
+            )
+
+        process = (
+            db.query(ChiTietXuLyRac)
+            .filter_by(maVideo=id_video, maRacThai=id_waste)
+            .first()
+        )
+        if not process:
+            return JSONResponse(
+                content={"status": 404, "message": "Bảng không tồn tại."},
+                status_code=404,
+            )
+
+        if "ghiChu" in data:
+            process.ghiChu = data["ghiChu"]
+
+        # Ghi cập nhật vào database
+        db.commit()
+
+        return JSONResponse(
+            content={
+                "status": 200,
+                "message": "Cập nhật mô tả thành công.",
+                "data": {
+                    "ghiChu": process.ghiChu,
+                },
+            },
+            status_code=200,
+        )
+
+    except Exception as e:
+        return JSONResponse(
+            content={"status": 500, "message": f"Lỗi hệ thống: {str(e)}"},
+            status_code=500,
+        )
