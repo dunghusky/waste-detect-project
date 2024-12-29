30981f351407b87a1f3c0adb20956a24c0773a9e
DangThiThuyDung06 <thuydungaelita@gmail.com>
2024-12-29 21:00:17 +0700

    test

--- test_2.py
+++ test_2.py
@@ -8,23 +8,23 @@
 # # uploaf = convert_mp4(file_path)
 
 # file = _create_file.return_file_name(file_path)
-# print(file)
-# out_put = _upload_video_s3.upload_video_to_s3_async(uploaf)
-# from supervision import Point
-# LINE_START = Point(670, 0)
-# print(LINE_START)
-# LINE_END = Point(670, 750)
+# # print(file)
+# # out_put = _upload_video_s3.upload_video_to_s3_async(uploaf)
+# # from supervision import Point
+# # LINE_START = Point(670, 0)
+# # print(LINE_START)
+# # LINE_END = Point(670, 750)
 
-from yolo_model.controllers import _trigger_to_db
-from database.dependencies.dependencies import get_db
-from database.models.DanhMucPhanLoaiRac import DanhMucPhanLoaiRac
-from database.models.RacThai import RacThai
-from database.dependencies.dependencies import get_db
-from database.models.Camera import Camera
-from database.models.DanhMucMoHinh import DanhMucMoHinh
-from database.models.VideoXuLy import VideoXuLy
-from database.models.ChiTietXuLyRac import ChiTietXuLyRac
-from yolo_model.controllers import _save_to_db
+# from yolo_model.controllers import _trigger_to_db
+# from database.dependencies.dependencies import get_db
+# from database.models.DanhMucPhanLoaiRac import DanhMucPhanLoaiRac
+# from database.models.RacThai import RacThai
+# from database.dependencies.dependencies import get_db
+# from database.models.Camera import Camera
+# from database.models.DanhMucMoHinh import DanhMucMoHinh
+# from database.models.VideoXuLy import VideoXuLy
+# from database.models.ChiTietXuLyRac import ChiTietXuLyRac
+# from yolo_model.controllers import _save_to_db
 
 # id = _trigger_to_db.get_id_waste_from_class_name("chai-nhua")
 # print(id)
@@ -46,11 +46,20 @@ from yolo_model.controllers import _save_to_db
 # _save_to_db.save_details_wastes_process_db('1', '1', 10, )
 # print(id)
 
-from yolo_model.controllers import _upload_s3
+# from yolo_model.controllers import _upload_s3
 
-file = "./file_path/video_stream/output_20241204_211047_converted.mp4"
+# file = "./file_path/video_stream/output_20241204_211047_converted.mp4"
 
-up = _upload_s3.upload_img_to_s3(file)
-print("\n ", up)
-test = _upload_s3.convert_cloudfront_link(up)
-print(test)
+# up = _upload_s3.upload_img_to_s3(file)
+# print("\n ", up)
+# test = _upload_s3.convert_cloudfront_link(up)
+# print(test)
+
+import requests
+
+url = "https://fae9-113-22-86-22.ngrok-free.app/1"
+headers = {"ngrok-skip-browser-warning": "true"}
+
+response = requests.get(url, headers=headers)
+# print(response.text)
+# print("aaaaaaaaaa")
