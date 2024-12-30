30104b6094525cfbc3d07a5a5b27b267cfb60ed4
DangThiThuyDung06 <thuydungaelita@gmail.com>
2024-12-30 22:23:18 +0700

    test

--- database/connect_db/_database_mysql.py
+++ database/connect_db/_database_mysql.py
@@ -5,8 +5,16 @@ from config._config import settings
 # Chuỗi kết nối tới MySQL
 SQLALCHEMY_DATABASE_URL = settings.database_url
 
-# Tạo engine kết nối tới MySQL, không cần tham số encoding
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# Thêm connect_timeout và các thông số kết nối khác
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={
+        "connect_timeout": 3000,  # Thời gian timeout khi kết nối (giây)
+        "autocommit": True,  # Tùy chọn thêm nếu cần
+    },
+    pool_pre_ping=True,  # Tự động kiểm tra kết nối trước khi sử dụng
+    pool_recycle=28000,  # Tái sử dụng kết nối sau thời gian nhất định
+)
 
 # Tạo session factory
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
