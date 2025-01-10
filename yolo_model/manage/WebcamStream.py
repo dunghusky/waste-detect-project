90089e478d8f35c855aab2c8bc830ac9b205caba
DangThiThuyDung06 <thuydungaelita@gmail.com>
2025-01-10 23:53:22 +0700

    test

--- yolo_model/manage/WebcamStream.py
+++ yolo_model/manage/WebcamStream.py
@@ -58,76 +58,32 @@ import cv2
 import time
 
 
-# class WebcamStream:
-#     def __init__(self, stream_id=0):
-#         self.stream_id = stream_id  # default is 0 for primary camera
-
-#         # opening video capture stream
-#         self.vcap = cv2.VideoCapture(self.stream_id)
-#         if not self.vcap.isOpened():
-#             print("[Exiting]: Error accessing webcam stream.")
-#             exit(0)
-#         fps_input_stream = int(self.vcap.get(cv2.CAP_PROP_FPS))
-#         print("FPS of webcam hardware/input stream: {}".format(fps_input_stream))
-
-#         # reading a single frame from vcap stream for initializing
-#         self.grabbed, self.frame = self.vcap.read()
-#         if not self.grabbed:
-#             print("[Exiting] No more frames to read")
-#             exit(0)
-
-#         # self.stopped is set to False when frames are being read from self.vcap stream
-#         self.stopped = True
-
-#         # reference to the thread for reading next available frame from input stream
-#         self.t = Thread(target=self.update, args=())
-#         self.t.daemon = True  # daemon threads keep running in the background while the program is executing
-
-#         # Adding a lock to manage access to the frame
-#         self.lock = Lock()
-
-#     def start(self):
-#         self.stopped = False
-#         self.t.start()
-
-#     def update(self):
-#         while not self.stopped:
-#             grabbed, frame = self.vcap.read()
-#             if not grabbed or self.stopped:  # Thoát nếu không có frame hoặc nhận tín hiệu dừng
-#                 print("[Exiting] No more frames to read or stopped signal received")
-#                 break
-#             with self.lock:
-#                 self.frame = frame
-#                 self.new_frame = True  # Đánh dấu frame mới đã được đọc
-#         self.vcap.release()
-
-#     def read(self):
-#         with self.lock:
-#             if self.new_frame:
-#                 self.new_frame = False  # Đánh dấu frame đã được sử dụng
-#                 return self.frame
-#             return None  # Nếu không có frame mới, trả về None
-
-
-#     def stop(self):
-#         self.stopped = True
-
 class WebcamStream:
     def __init__(self, stream_id=0):
-        self.stream_id = stream_id
+        self.stream_id = stream_id  # default is 0 for primary camera
+
+        # opening video capture stream
         self.vcap = cv2.VideoCapture(self.stream_id)
         if not self.vcap.isOpened():
             print("[Exiting]: Error accessing webcam stream.")
             exit(0)
+        fps_input_stream = int(self.vcap.get(cv2.CAP_PROP_FPS))
+        print("FPS of webcam hardware/input stream: {}".format(fps_input_stream))
 
+        # reading a single frame from vcap stream for initializing
         self.grabbed, self.frame = self.vcap.read()
         if not self.grabbed:
             print("[Exiting] No more frames to read")
             exit(0)
 
+        # self.stopped is set to False when frames are being read from self.vcap stream
         self.stopped = True
+
+        # reference to the thread for reading next available frame from input stream
         self.t = Thread(target=self.update, args=())
-        self.t.daemon = True
+        self.t.daemon = True  # daemon threads keep running in the background while the program is executing
+
+        # Adding a lock to manage access to the frame
         self.lock = Lock()
 
     def start(self):
@@ -137,26 +93,24 @@ class WebcamStream:
     def update(self):
         while not self.stopped:
             grabbed, frame = self.vcap.read()
-            if not grabbed:
-                print("[Exiting] No more frames to read")
-                self.stopped = True
+            if not grabbed or self.stopped:  # Thoát nếu không có frame hoặc nhận tín hiệu dừng
+                print("[Exiting] No more frames to read or stopped signal received")
                 break
-
             with self.lock:
-                self.frame = frame  # Chỉ lưu frame mới nhất (loại bỏ frame cũ)
-
+                self.frame = frame
+                self.new_frame = True  # Đánh dấu frame mới đã được đọc
         self.vcap.release()
 
     def read(self):
         with self.lock:
-            return self.frame  # Trả về frame mới nhất
+            if self.new_frame:
+                self.new_frame = False  # Đánh dấu frame đã được sử dụng
+                return self.frame
+            return None  # Nếu không có frame mới, trả về None
+
 
     def stop(self):
         self.stopped = True
-        if self.t.is_alive():
-            self.t.join()
-
-
 
 
 # class WebcamStream:
